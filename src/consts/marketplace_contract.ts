29fb5cc7dbecc3c500b0b6378b3485739989f62f
nftname <nftnamemarket@gmail.com>
2025-12-24 13:57:10 +0000

    Fix contracts structure to match app requirements

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -1,10 +1,15 @@
-import { getContract } from "thirdweb";
-import { client } from "./client";
+import type { Chain } from "thirdweb";
 import { exampleChain } from "./chains";
 
+type MarketplaceContract = {
+  address: string;
+  chain: Chain;
+};
+
 // My Polygon Marketplace Contract
-export const marketplaceContract = getContract({
+export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
+  {
     address: "0xbc1e247e6eec6c179f77b6E28A0c90609EB1c4EB",
     chain: exampleChain,
-    client,
-});
+  },
+];
--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -1,10 +1,24 @@
-import { getContract } from "thirdweb";
-import { client } from "./client";
+import type { Chain } from "thirdweb";
 import { exampleChain } from "./chains";
 
+export type NftContract = {
+  address: string;
+  chain: Chain;
+  type: "ERC1155" | "ERC721";
+
+  title?: string;
+  description?: string;
+  thumbnailUrl?: string;
+  slug?: string;
+};
+
 // My Polygon NFT Collection Contract
-export const nftContract = getContract({
+export const NFT_CONTRACTS: NftContract[] = [
+  {
     address: "0x8e46c897bc74405922871a8a6863ccf5cd1fc721",
     chain: exampleChain,
-    client,
-});
+    type: "ERC721",
+    title: "My NFT Collection",
+    description: "My Polygon NFT Collection",
+  },
+];
